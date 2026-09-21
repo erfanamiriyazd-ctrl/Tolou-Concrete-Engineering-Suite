6707a013bb363f9f1b64cfce6c9b231f41913800
mixlinead-creator <mixlinead@gmail.com>
2026-09-21 04:08:30 +0330

    Expose workspace persistence bridge

--- src/preload.cjs
+++ src/preload.cjs
@@ -1,5 +1,9 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('tolouDesktop', {
-  getAppInfo: () => ipcRenderer.invoke('tolou:getAppInfo')
+  getAppInfo: () => ipcRenderer.invoke('tolou:getAppInfo'),
+  loadWorkspace: () => ipcRenderer.invoke('tolou:workspace:load'),
+  saveWorkspace: (data) => ipcRenderer.invoke('tolou:workspace:save', data),
+  createBackup: (label) => ipcRenderer.invoke('tolou:workspace:backup', label),
+  restoreBackup: (backupPath) => ipcRenderer.invoke('tolou:workspace:restore', backupPath)
 });
