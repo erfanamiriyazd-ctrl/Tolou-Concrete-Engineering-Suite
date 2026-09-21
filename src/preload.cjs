6e30aeedf141a0b7e514ebb276140837bb7b9bbb
mixlinead-creator <mixlinead@gmail.com>
2026-09-21 04:12:21 +0330

    Install renderer localStorage persistence sync

--- src/preload.cjs
+++ src/preload.cjs
@@ -1,9 +1,14 @@
 const { contextBridge, ipcRenderer } = require('electron');
+const { installLocalStorageSync } = require('./renderer-sync.cjs');
 
-contextBridge.exposeInMainWorld('tolouDesktop', {
+const desktopApi = {
   getAppInfo: () => ipcRenderer.invoke('tolou:getAppInfo'),
   loadWorkspace: () => ipcRenderer.invoke('tolou:workspace:load'),
   saveWorkspace: (data) => ipcRenderer.invoke('tolou:workspace:save', data),
   createBackup: (label) => ipcRenderer.invoke('tolou:workspace:backup', label),
   restoreBackup: (backupPath) => ipcRenderer.invoke('tolou:workspace:restore', backupPath)
-});
+};
+
+contextBridge.exposeInMainWorld('tolouDesktop', desktopApi);
+
+installLocalStorageSync(window, desktopApi);
